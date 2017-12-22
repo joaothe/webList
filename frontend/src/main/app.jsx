882c6c1535cedef0caac33f15e5d067eb9023a89
Joao Paulo <joaoded@hotmail.com>
2017-12-21 22:37:41 -0200

    New components to keep building the project

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,11 @@
 
 import React from 'react'
+import Todo from '../todo/todo'
+import About from '../about/about'
 
 export default props => (
   <div className=''>
-    <h1>Teste</h1>
+    <Todo/>
+    <About/>
   </div>
 )
