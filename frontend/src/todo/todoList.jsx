971eb7dc5e13136c32d2c9d382b7d08a8fc6eece
Joao Paulo <joaoded@hotmail.com>
2018-06-04 15:23:04 -0300

    Basics styles additions

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -6,9 +6,9 @@ export default props => {
     return list.map(todo => (
       <tr key={todo._id}>
         <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
-        <td><button onClick={() => props.handleMarkAsDone(todo)}>Marcar como feito</button></td>
-        <td><button onClick={() => props.handleMarkAsPending(todo)}>Marcar como pendente</button></td>
-        <td><button onClick={() => props.handleRemove(todo)}>Remover</button></td>
+        <td><button className="styledButton" onClick={() => props.handleMarkAsDone(todo)}>Marcar como feito</button></td>
+        <td><button className="styledButton" onClick={() => props.handleMarkAsPending(todo)}>Marcar como pendente</button></td>
+        <td><button className="styledButton" onClick={() => props.handleRemove(todo)}>Remover</button></td>
       </tr>
     ))
   }
