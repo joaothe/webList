9e384fea22906e06912af73e8beef011b20b590c
Joao <joaoded@hotmail.com>
2018-06-22 23:37:35 -0300

    Enabling some keyboard shortcut for adding and searching tasks in the form

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
-export default props => (
-  <div role='form'>
-    <div>
-      <input id='description' placeholder='Adicione uma tarefa' onChange={props.handleChange}
-        value={props.description}></input>
+export default props => {
+
+  const keyHandler = (e) => {
+    if (e.key === 'Enter') {
+      e.shiftKey ? props.handleSearch() : props.handleAdd()
+    }
+  }
+
+  return (
+    <div role='form'>
+      <div>
+        <input id='description' placeholder='Adicione uma tarefa' onChange={props.handleChange}
+          onKeyUp={keyHandler}
+          value={props.description}></input>
+      </div>
+      <div>
+        <button onClick={props.handleAdd}>Adicionar</button>
+        <button onClick={props.handleSearch}>Pesquisar</button>
+      </div>
     </div>
-    <div>
-      <button onClick={props.handleAdd}>Adicionar</button>
-      <button onClick={props.handleSearch}>Pesquisar</button>
-    </div>
-  </div>
-)
+  )
+}
