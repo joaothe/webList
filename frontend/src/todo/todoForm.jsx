23b7c544aca7e8211e6dfbd470e4606dad6b044c
Joao <joaoded@hotmail.com>
2018-06-25 18:01:34 -0300

    Created search action

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { changeDescription } from './todoActions'
+import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
+    this.keyHandler = this.keyHandler.bind(this)
+  }
 
-  const keyHandler = (e) => {
+  componentWillMount() {
+    this.props.search()
+  }
+
+  keyHandler(e) {
     if (e.key === 'Enter') {
-      e.shiftKey ? props.handleSearch() : props.handleAdd()
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
     }
   }
 
-  return (
-    <div role='form'>
-      <div>
-        <input id='description' placeholder='Adicione uma tarefa' onChange={props.changeDescription}
-          onKeyUp={keyHandler}
-          value={props.description}></input>
+  render() {
+    return (
+      <div role='form'>
+        <div>
+          <input id='description' placeholder='Adicione uma tarefa' onChange={this.props.changeDescription}
+          onKeyUp={this.keyHandler}
+          value={this.props.description}></input>
+        </div>
+        <div>
+          <button onClick={this.props.handleAdd}>Adicionar</button>
+          <button onClick={this.props.handleSearch}>Pesquisar</button>
+        </div>
       </div>
-      <div>
-        <button onClick={props.handleAdd}>Adicionar</button>
-        <button onClick={props.handleSearch}>Pesquisar</button>
-      </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ changeDescription }, dispatch)
+    bindActionCreators({ changeDescription, search }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
