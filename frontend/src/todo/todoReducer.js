5117fedb66310972fe348458981d58581b323367
Joao <joaoded@hotmail.com>
2018-06-25 22:39:12 -0300

    Using middleware to work ok with action payload

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,18 +1,6 @@
 const INITIAL_STATE = {
-    description: 'To study',
-    list: [{
-        _id: 1,
-        description: 'Read new books',
-        done: true
-    },{
-        _id: 2,
-        description: 'Meeting with the team',
-        done: false
-    },{
-        _id: 3,
-        description: 'Make new notes',
-        done: false
-    }]
+    description: '',
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
